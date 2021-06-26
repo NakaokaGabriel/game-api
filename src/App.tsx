8f8f3fcacf02cfc344172c9636e237a36e58903d
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2021-06-26 18:00:30 -0300

    create dark background

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './styles/tailwind.css';
 
 import Routes from './routes';
 
 function App() {
+  useEffect(() => {
+    const root = window.document.documentElement;
+
+    root.classList.add('dark');
+  }, []);
+
   return (
-    <Routes />
+    <div className="bg-white dark:bg-gray-900 h-screen overflow-y-auto text-gray-50">
+      <Routes />
+    </div>
   );
 }
 
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const HomePage = () => {
   return (
-    <h1>Home page</h1>
+    <>
+      <h1>Home page</h1>
+    </>
   );
 }
 
