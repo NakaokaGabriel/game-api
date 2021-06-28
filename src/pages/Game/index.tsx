5c7223cd769b371f4b28aaeeb19e9d2e8329ac44
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2021-06-28 01:20:34 -0300

    style game page

--- src/pages/Game/index.tsx
+++ src/pages/Game/index.tsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom';
+import { FaChevronLeft } from 'react-icons/fa';
+import { useParams, Link } from 'react-router-dom';
+
+import Navigation from '../../components/Navigation';
 
 import { api } from '../../services/api';
 import { Developers, Genres, Plataforms } from '../../types/Games';
@@ -31,7 +34,31 @@ const Game = () => {
 
   return (
     <div className="container mx-auto px-2">
-      <img src={gameData?.background_image} alt={gameData?.name} />
+      <Navigation />
+
+      <div className="mt-20">
+        <Link to="/" className="flex items-center">
+          <div className="bg-gray-700 p-2 rounded-full flex items-center justify-center">
+            <FaChevronLeft color="#fff" />
+          </div>
+          <p className="ml-3">Back to all games</p>
+        </Link>
+      </div>
+
+      <div className="block rounded relative overflow-hidden mt-3 h-96">
+        <img
+          src={gameData?.background_image}
+          alt={gameData?.name}
+        />
+
+        <div className="absolute bottom-0 left-0 w-full h-16 backdrop-filter backdrop-blur-sm rounded-none" />
+
+        <div className="absolute bottom-0 left-0 w-full h-16 bg-gray-900 bg-opacity-70 flex items-center">
+          <p className="ml-3 font-bold drop-shadow-2xl">
+            {gameData?.name}
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
